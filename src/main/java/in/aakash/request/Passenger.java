9640e81b004b9669ef3f2814326d3ad7df6b90c6
Aakash Patel <aakashpatelsky11@gmail.com>
2025-01-10 00:22:39 +0530

    Train Ticket App Test Using Postman learn concept of consumer & produces

---
+++ src/main/java/in/aakash/request/Passenger.java
@@ -0,0 +1,15 @@
+package in.aakash.request;
+
+import lombok.Data;
+
+@Data
+public class Passenger {
+	
+	private String fname;
+	private String lname;
+	private String gender;
+	private String from;
+	private String to;
+	private String doj;
+	private String traiNum;
+}
---
+++ src/main/java/in/aakash/response/Ticket.java
@@ -0,0 +1,18 @@
+package in.aakash.response;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import lombok.Data;
+
+@Data
+@XmlRootElement
+public class Ticket {
+
+	private Integer ticketId;
+	private String ticketStatus;
+	private String ticketPrice;
+	private String from;
+	private String to;
+	private String trainNum;
+	private String doj;
+}
